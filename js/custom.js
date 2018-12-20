a0c2dacc94e3739eb287045f0be60ba723418fc9
cleitonnudo <cleiton_nudo@hotmail.com>
2018-12-20 17:50:48 -0200

    update clima

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 ﻿(function() {
     'use strict';
-    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-showers-heavy'></span><h3>Chuva pra Caraleo</h3><h4>Nesse momento,</h4><h5>última atualização 20/12 - 15:40</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
+    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-showers-heavy'></span><h3>Chuva pra Caraleo</h3><h4>Nesse momento,</h4><h5>última atualização 20/12 - 17:50</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
     $(".button-popup").mouseover(function(){
       $(".tempo-popup").css('right','0');
     });
