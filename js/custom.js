298a395ff28ceb73dbe268a1dc2f8d61211314f4
cleitonnudo <cleiton_nudo@hotmail.com>
2018-12-20 14:48:42 -0200

    Update Clima

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-(function() {
+﻿(function() {
     'use strict';
-    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-sun'></span><h3>Sol entre nuvens</h3><h4>Nesse momento,</h4><h5>última atualização 20/12 - 13:50</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
+    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud'></span><h3>Nublado</h3><h4>Nesse momento,</h4><h5>última atualização 20/12 - 15:00</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
     $(".button-popup").mouseover(function(){
       $(".tempo-popup").css('right','0');
     });
