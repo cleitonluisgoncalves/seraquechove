87c1ce1a3e11dbaf51ae12e18052019038dce881
cleitonnudo <cleiton_nudo@hotmail.com>
2018-12-20 11:31:45 -0200

    First commit
    First commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-sun'></span><h3>Calor pra corói</h3><h4>Na firma ta</h4><h5>última atualização 20/12 - 10:00</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
+    $(".button-popup").mouseover(function(){
+      $(".tempo-popup").css('right','0');
+    });
+    $(".tempo-popup").mouseleave(function(){
+        $(".tempo-popup").css('right','-200px');
+    });
+})();
