3785df7035bbbf07cc6784041a37c43e04cd3626
cleitonnudo <cleiton_nudo@hotmail.com>
2018-12-21 10:50:09 -0200

    button effects

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,12 @@
     'use strict';
     $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-showers-heavy'></span><h3>Chuva pra Caraleo</h3><h4>Nesse momento,</h4><h5>última atualização 21/12 - 10:50</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
     $(".button-popup").mouseover(function(){
-      $(".tempo-popup").css('right','0');
+      $(".tempo-popup").css({'right' : '0'});
+      $(".button-popup").css({'left' : '0', 'opacity' : '0.0' });
     });
     $(".tempo-popup").mouseleave(function(){
         $(".tempo-popup").css('right','-200px');
+	$(".button-popup").css({'left' : '-35px', 'opacity' : '1' });
     });
 })();
 var newCSS = GM_getResourceText ("seraquechovestyle");
