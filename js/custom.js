985fc5a4845363bd87e7ecdd454f5fd2b778ce8a
cleitonnudo <cleiton_nudo@hotmail.com>
2019-01-03 15:07:52 -0200

    update clima

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 ﻿(function() {
     'use strict';
-    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-showers-heavy'></span><h3>Chuvão</h3><h4>Nesse momento,</h4><h5>última atualização 03/01- 15:00</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
+    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-sun-rain'></span><h3>Chuva e Sol</h3><h4>Nesse momento,</h4><h5>última atualização 03/01- 15:00</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
     $(".button-popup").mouseover(function(){
       $(".tempo-popup").css({'right' : '0'});
       $(".button-popup").css({'left' : '0', 'opacity' : '0.0' });
