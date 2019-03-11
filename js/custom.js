d5fe2b5f28aa5f6fd39cc3f9dfde3295103dc1dc
cleitonnudo <cleiton_nudo@hotmail.com>
2019-03-11 10:54:31 -0300

    Tempo Off
    Desabilitando a previsao do tempo

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,1 @@
-﻿(function() {
-    'use strict';
-    $("body").append("<div class='tempo-popup'><div class='content-popup'><span class='fas fa-cloud-sun-rain'></span><h3>Chuva e Sol</h3><h4>Nesse momento,</h4><h5>última atualização 03/01- 15:00</h5></div><div class='button-popup'><span class='fas fa-meteor'></span></div></div>");
-    $(".button-popup").mouseover(function(){
-      $(".tempo-popup").css({'right' : '0'});
-      $(".button-popup").css({'left' : '0', 'opacity' : '0.0' });
-    });
-    $(".tempo-popup").mouseleave(function(){
-        $(".tempo-popup").css({'right' : '-200px'});
-	$(".button-popup").css({'left' : '-35px', 'opacity' : '1' });
-    });
-})();
-var newCSS = GM_getResourceText ("seraquechovestyle");
-GM_addStyle (newCSS);
+﻿
