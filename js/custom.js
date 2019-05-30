2f0f378922f17a0a1029465524247d0bd223d986
cleitonnudo <cleiton_nudo@hotmail.com>
2019-05-30 16:25:33 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,21 @@ $(document).ready(function () {
     
     var imgId = getRandomInt();
     var urlImg = "url(https://images.pexels.com/photos/"+ imgId + "/pexels-photo-"+ imgId +".jpeg)";
+    var urlCore = "url(https://images.pexels.com/photos/"+ imgId + "/pexels-photo-"+ imgId +".jpeg";
+
+    function checkOnline(url, error, ok) {
+        try {
+            var scriptElem = document.createElement('script');
+            scriptElem.type = 'text/javascript';
+            scriptElem.onerror = function(){error();};
+            scriptElem.onload = function(){ok();};
+            scriptElem.src = url;
+            document.getElementsByTagName("body")[0].appendChild(scriptElem);
+        } catch(err) {
+            error(err);
+        }
+    };
+   console.log(checkOnline(urlCore))
 
     $(".oc").css("background", urlImg);
     $(".oc").css("background-size", "cover");
