0940a487081592033dcc24fbb16851b1f5d4ddf5
cleitonnudo <cleiton_nudo@hotmail.com>
2019-05-30 12:00:48 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
 ﻿
+$(document).ready(function () {
+    function getRandomInt(min, max) {
+        min = 1000
+        max = 2000
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    console.log(getRandomInt());
+});
